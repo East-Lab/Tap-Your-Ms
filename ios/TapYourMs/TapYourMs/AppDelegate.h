a80b187cb955c78ea79371618615da9ec3937ea9
Kawashima Daichi <daichi0.0daichi@gmail.com>
2015-02-28 15:27:43 +0900

    first commit

---
+++ ios/TapYourMs/TapYourMs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapYourMs
+//
+//  Created by 川島 大地 on 2015/02/28.
+//  Copyright (c) 2015年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/TapYourMs/TapYourMs/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TapYourMs
+//
+
+//  Copyright (c) 2015年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
